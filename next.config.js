2e05e0f9d2325ae835787f296680ca582692a9f5
farrel-a <13520110@std.stei.itb.ac.id>
2023-12-18 18:40:03 +0700

    fix: remove strict mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
     WS_ROSBRIDGE_URL: process.env.WS_ROSBRIDGE_URL,
